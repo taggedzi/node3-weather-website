27fdd680c2e1915a4cdf04f2cb653b2b98e30f09
Matthew Craig <taggedzi.mpc@gmail.com>
2022-02-28 15:07:03 -0500

    Changes to allow running in heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forcast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -94,9 +95,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`)
 })
-
-// C:\Users\tagge\Google Drive\Documents\Class and Book Notes\The Complete Nodejs Dev course\projects\web-server\src
-// C:\Users\tagge\Google Drive\Documents\Class and Book Notes\The Complete Nodejs Dev course\projects\web-server\src\app.js
